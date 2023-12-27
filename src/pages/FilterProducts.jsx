5eddad30f7355eb4b818ab23423bb119fce47be3
saurav-saksena <sauravkumar4363@gmail.com>
2023-12-27 18:37:27 +0530

    added filter functionality with maincategory and brand name filteration

--- src/pages/FilterProducts.jsx
+++ src/pages/FilterProducts.jsx
@@ -2,26 +2,43 @@ import React, { useEffect, useState } from 'react'
 
 export default function FilterProducts() {
     const [products, setProducts] = useState([])
+    const [productLists, setProductLists] = useState([])
     const [filteredProducts, setFilteredProducts] = useState([])
     const [selectedCategory, setSelectedCategory] = useState([])
+    const [msg, setMsg] = useState("all")
 
+    const maincategoryBtn = ["all", ...new Set(products.map(irem => irem.maincategory))]
     const filterbutton = [...new Set(products.map(item => item.brand))]
     // console.log(filterbutton)
+    const handleMaincategory = (item) => {
+        if (item === "all") {
+            setProductLists(products)
+        } else {
+            setProductLists(products.filter(all => all.maincategory === item))
+        }
+        setMsg(item)
+    }
     const handleFilter = (item) => {
         addCategory(item)
     }
     // console.log(selectedCategory)
     const getApi = async () => {
-        const response = await fetch("http://localhost:8000/api/product", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        const result = await response.json()
-        // console.log(response.data)
-        setProducts(result.data)
-        setFilteredProducts(result.data)
+        try {
+            const response = await fetch("http://localhost:8000/api/product", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            const result = await response.json()
+            // console.log(response.data)
+            setProducts(result.data)
+            setProductLists(result.data)
+            setFilteredProducts(result.data)
+        } catch (error) {
+            console.log(error)
+        }
+
     }
     const addCategory = (item) => {
         if (!selectedCategory.includes(item)) {
@@ -34,9 +51,9 @@ export default function FilterProducts() {
     // console.log(selectedCategory)
     const filterProductWithCategory = () => {
         if (selectedCategory.length === 0) {
-            setFilteredProducts(products)
+            setFilteredProducts(productLists)
         } else {
-            setFilteredProducts(products.filter(item => selectedCategory.includes(item.brand)))
+            setFilteredProducts(productLists.filter(item => selectedCategory.includes(item.brand)))
         }
     }
     const clearFilter = () => {
@@ -45,7 +62,7 @@ export default function FilterProducts() {
     useEffect(() => {
         filterProductWithCategory()
         // eslint-disable-next-line
-    }, [selectedCategory, products.length])
+    }, [selectedCategory, productLists])
 
     useEffect(() => {
         getApi()
@@ -53,15 +70,20 @@ export default function FilterProducts() {
     }, [])
     return (
         <div className='white--ground'>
+            {products.length !== 0 && maincategoryBtn.map(item => {
+                return <button key={item} onClick={() => handleMaincategory(item)}>{item}</button>
+            })}
+            {msg !== "all" && <span className='selected-msg'>selected Category: {msg}</span>}
             <div className='filter-button'>
-                {products.length &&
+                {products.length !== 0 &&
                     filterbutton.map((item, i) => {
                         return (
                             <span onClick={() => handleFilter(item)} className={selectedCategory.includes(item) ? "fitler-button-span1" : "fitler-button-span"} key={item}>{item}</span>
                         )
                     })
                 }
-                <span onClick={clearFilter} className={selectedCategory.length === 0 ? "cleared-filter" : "clear-filter"}>clear_filter</span>
+                {/* <span onClick={clearFilter} className={selectedCategory.length === 0 ? "cleared-filter" : "clear-filter"}>clear_filter</span> */}
+                {selectedCategory.length !== 0 && <span className='clear-filter' onClick={clearFilter}>clear_filter</span>}
             </div>
 
             <div className='filter-product-container'>
